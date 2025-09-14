76f3d2a1b0414986c9671754cd5e0d50d6140c55
Nenad Vukmirovic <nenad@Nenads-iMac.local>
2025-09-14 19:45:23 +0200

    Update

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+const appInstalation  = require('./installation/app-installation');
 
 const app = express();
+app.get('/oauth-callback', appInstalation);
 
 const PORT = process.env.PORT || 3000;
 
