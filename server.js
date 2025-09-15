c741fe52dea2a24c31e71e6411865e8c43d2f0e0
Nenad Vukmirovic <nenad@Nenads-iMac.local>
2025-09-15 17:50:06 +0200

    Update

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.use(express.json());
 // Serve static frontend files
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.post('/webhook', (req, res) => {
+  const events = req.body;
+
+  if (!Array.isArray(events) || events.length === 0) {
+      console.error("Webhook Error: Empty or invalid payload");
+      return res.status(400).send('Bad Request');
+  }
+  console.log(events);
+  res.status(200).send('Webhook processed');
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is listening on this port ${PORT}`);
