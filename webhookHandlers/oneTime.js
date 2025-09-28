e6bd7d2fe2513ca433544a380c79ac2eb548fc81
Nenad Vukmirovic <nenad@Nenads-iMac.local>
2025-09-28 22:48:35 +0200

    Update

--- webhookHandlers/oneTime.js
+++ webhookHandlers/oneTime.js
@@ -1,3 +1,5 @@
+const Stripe = require('stripe');
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const axios = require('axios');
 const setHubSpotToken = require('../database/getTokens');
 
